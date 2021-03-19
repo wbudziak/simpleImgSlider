c353aff3981ca0ae65bdb48fbc83c72937292d4f
Wojciech Budziak <steamvexlex@gmail.com>
2021-03-19 20:19:39 +0100

    add slide show

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const imgBox = document.querySelector('.container__img-box');
 const image = [...document.querySelectorAll('.container__img')];
 const arrowLeft = document.querySelector('.container__arrow-left');
 const arrowRight = document.querySelector('.container__arrow-right');
+const slideBtn = document.querySelector('.container__slide-btn');
 const spanNumber = document.querySelector('span');
 let movePX = 0;
 let number = 1;
+let slideInterval;
+let slideTransition = 3000;
 
 const start = () => {
     imgBox.style.width = `${image.length * 100}%`;
     spanNumber.textContent = `Picture ${number}/${image.length}`;
 }
 const numberOfPicture = () => {
-    spanNumber.textContent = `Picture ${number} of ${image.length}`;
+    spanNumber.textContent = `Image ${number} of ${image.length}`;
 }
 const animation = () => {
     imgBox.style.transition = "0s linear all";
@@ -23,11 +26,7 @@ const animation = () => {
         imgBox.style.transition = ".3s linear all";
     }, 300);
 }
-start();
-numberOfPicture();
-
-
-btnLeft.addEventListener('click', (e) => {
+const slideLeft = () => {
     if (number <= 1) {
         animation()
         number = image.length;
@@ -40,16 +39,8 @@ btnLeft.addEventListener('click', (e) => {
     numberOfPicture();
     movePX += 100;
     imgBox.style.left = `${movePX}%`;
-})
-btnLeft.addEventListener("mousedown", ()=>{
-    arrowLeft.style.transform = "translateX(-5px)";
-})
-btnLeft.addEventListener("mouseup", ()=>{
-    arrowLeft.style.transform = "translateX(0px)";
-})
-
-
-btnRight.addEventListener('click', (e) => {
+}
+const slideRight = () => {
     arrowLeft.style.transform = "translateX(-5px);";
     if (number >= image.length) {
         animation()
@@ -63,10 +54,48 @@ btnRight.addEventListener('click', (e) => {
     numberOfPicture();
     movePX -= 100;
     imgBox.style.left = `${movePX}%`;
+}
+
+btnLeft.addEventListener('click', slideLeft);
+
+btnLeft.addEventListener("mousedown", () => {
+    arrowLeft.style.transform = "translateX(-5px)";
 })
-btnRight.addEventListener("mousedown", ()=>{
+
+btnLeft.addEventListener("mouseup", () => {
+    arrowLeft.style.transform = "translateX(0px)";
+})
+
+btnRight.addEventListener('click', slideRight);
+
+btnRight.addEventListener("mousedown", () => {
     arrowRight.style.transform = "translateX(5px)";
 })
-btnRight.addEventListener("mouseup", ()=>{
+
+btnRight.addEventListener("mouseup", () => {
     arrowRight.style.transform = "translateX(0px)";
 })
+let slideFlag = true;
+
+slideBtn.addEventListener('click', () => {
+
+    if (slideFlag) {
+        slideFlag = !slideFlag;
+        slideBtn.innerHTML = '<i class="fas fa-pause"></i>';
+        slideInterval = setInterval(() => {
+            slideRight();
+            if (number === 1) {
+                window.clearInterval(slideInterval);
+                slideBtn.innerHTML = `<i class="fas fa-play"></i>`;
+                slideFlag = !slideFlag;
+            }
+        }, slideTransition);
+    } else {
+        slideFlag = !slideFlag;
+        window.clearInterval(slideInterval);
+        slideBtn.innerHTML = `<i class="fas fa-play"></i>`;
+    }
+})
+
+start();
+numberOfPicture();
