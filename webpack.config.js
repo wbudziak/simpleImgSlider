078b48ba928cb96fb3fe57d1bb72b670b3aeea24
Mikolaj <mikolaj@grid.gg>
2021-03-25 22:24:31 +0100

    Install and configure basic webpack live server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'index.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
     },
 };
