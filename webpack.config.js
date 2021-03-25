e559e48d656a2a04ab6aef42569cdb3cce1e8414
Mikolaj <mikolaj@grid.gg>
2021-03-25 22:58:00 +0100

    Create ts config files

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
         contentBase: './dist',
         port: 3000,
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    }
 };
